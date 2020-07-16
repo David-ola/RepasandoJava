a7ae05b2e8bdff16fb08b37ce521958313695689
Jaminito <sjamaldeite@gmail.com>
2020-07-16 20:22:17 +0200

    Academia creada

---
+++ src/com/company/Modelos/Academia.java
@@ -0,0 +1,32 @@
+package com.company.Modelos;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Academia {
+    private List<Alumno> academia;
+
+        public Academia() {
+            this.academia =new ArrayList<>();
+        }
+
+        public int numeroAlumnos(){
+            return this.academia.size();
+        }
+
+        public void matricularAlumno(String nombre,String apellido,Date nacimiento,String dni){
+            this.academia.add(new Alumno(nombre,apellido,nacimiento,dni));
+        }
+
+        public List<String> mostrarAlumnos() {
+            List<String> n = new ArrayList<>();
+            this.academia.forEach(alumno->{
+                System.out.println(alumno.toString());
+                System.out.println(alumno.getNombre());
+                n.add(alumno.getNombre() + " "+ alumno.getApellidos() +" "+alumno.getNacimiento()+" "+alumno.getDni());
+            });
+            return null;
+        }
+    }
+
