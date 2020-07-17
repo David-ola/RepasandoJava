97bb647e19d327c85112ec547055ea5bdbaaf60b
Tomas <tomas.fermoso@gmail.com>
2020-07-17 18:15:19 +0200

    Add Class Conexion

---
+++ src/com/company/bd/Conexion.java
@@ -0,0 +1,30 @@
+package com.company.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static String url="jdbc:mysql://localhost/academia";
+    private static String usr="root";
+    private static String pass="";
+    private static Connection conn;
+
+    public static Connection getConn() {
+        if(conn==null){
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            // Establecemos la conexión con la base de datos.
+            try {
+                conn = DriverManager.getConnection (url,usr,pass);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+}
+
